e8d40352b7d02c6ee2513b0fdee882f5be1ef808
Peter Yoachim <yoachim@uw.edu>
2020-02-13 10:38:59 -0800

    maf all should work

--- maf_all/generate_script.py
+++ maf_all/generate_script.py
@@ -8,9 +8,7 @@ if __name__ == "__main__":
     outfile = open("run_all.sh", 'w')
 
     for filename in db_files:
-        outfile.write('python ../glace_dir --db %s' % filename)
-        outfile.write('python ../scimaf_dir.py --db %s' % filename)
+        outfile.write('python ../glance_dir.py --db %s \n' % filename)
+        outfile.write('python ../scimaf_dir.py --db %s \n' % filename)
 
     outfile.close()
-
-
